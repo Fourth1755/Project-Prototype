0af568edfb4e40c10bb27ed1305c96a2d7f85c85
Fourth1755 <87474795+Fourth1755@users.noreply.github.com>
2022-05-27 14:14:51 +0700

    Update index.js

--- frontend/src/component/SideBar/index.js
+++ frontend/src/component/SideBar/index.js
@@ -11,23 +11,29 @@ const SideBar=()=>{
     const routes = [
         { path: '/app/monitors', title: 'Monitor', icon: 'fas fa-tv', id: 'mornitor' },
         { path: '/app/chats', title: 'Chat', icon: 'far fa-envelope', id: 'chat' },
-        {
-          title: 'Setting',
-          icon: 'fas fa-cog',
-          id: 'setting',
-          routes: [
-            { path: '/app/partners', title: 'Anime', icon: 'far fa-handshake', id: 'partners' },
-            { path: '/app/users', title: 'Studio', icon: 'far fa-user', id: 'users' },
-            { path: '/app/line', title: 'Actor', icon: 'fab fa-line', id: 'line_oa' },
-            { path: '/app/resolve', title: 'Song', icon: 'far fa-folder', id: 'job_type' }
-          ]
-        }
+        // {
+        //   title: 'Setting',
+        //   icon: 'fas fa-cog',
+        //   id: 'setting',
+        //   routes: [
+        //     { path: '/app/partners', title: 'Anime', icon: 'far fa-handshake', id: 'partners' },
+        //     { path: '/app/users', title: 'Studio', icon: 'far fa-user', id: 'users' },
+        //     { path: '/app/line', title: 'Actor', icon: 'fab fa-line', id: 'line_oa' },
+        //     { path: '/app/resolve', title: 'Song', icon: 'far fa-folder', id: 'job_type' }
+        //   ]
+        // }
       ]
     return(
         <div className="app-sidebar">
             <div className="routes">
             <List sx={{ width: '100%', maxWidth: 360 }} component="nav">
-
+              {routes.map((route,i)=>{
+                <ListItemButton>
+                  <div>
+                    <ListItemText primary={route.title}/>
+                  </div>
+                </ListItemButton>
+              })}
             </List>
             </div>
         </div>
